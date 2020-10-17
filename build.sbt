abd36bd9ae75884751a96969994dcb0765ae21e4
lukaszlepak <lukasz.lepak@protonmail.com>
2020-10-17 02:01:34 +0200

    Add files loading and saving

--- build.sbt
+++ build.sbt
@@ -3,3 +3,9 @@ name := "scala-image-processing"
 version := "0.1"
 
 scalaVersion := "2.13.3"
+
+libraryDependencies += "com.sksamuel.scrimage" % "scrimage-core" % "4.0.8"
+
+libraryDependencies += "com.sksamuel.scrimage" %% "scrimage-scala" % "4.0.8"
+
+libraryDependencies += "com.typesafe" % "config" % "1.4.0"
--- src/main/scala/DarkImageFilter.scala
+++ src/main/scala/DarkImageFilter.scala
@@ -1,3 +1,34 @@
+import java.io.File
+
+import com.sksamuel.scrimage.ImmutableImage
+import com.sksamuel.scrimage.nio.{JpegWriter, PngWriter}
+import com.typesafe.config.ConfigFactory
+
+
+case class Config(inputDirectory: String, outputDirectory: String)
+
+
 object DarkImageFilter extends App {
-  println("HELLO WORLD!")
+
+  val configFile = ConfigFactory.load("application.conf")
+
+  val config = Config(
+    configFile.getString("dark-image-filter.input-directory"),
+    configFile.getString("dark-image-filter.output-directory")
+  )
+
+  new File(config.inputDirectory).listFiles().foreach { file =>
+    val (name, extension) = file.getName.split('.') match {
+      case Array(n, e) => (n, e)
+    }
+
+    val image = ImmutableImage.loader().fromFile(file)
+
+    extension match {
+      case "jpg" => image.output(JpegWriter.Default, config.outputDirectory + s"/${name}_bright_0.jpg")
+      case "jpeg" => image.output(JpegWriter.Default, config.outputDirectory + s"/${name}_bright_0.jpeg")
+      case "png" => image.output(PngWriter.NoCompression, config.outputDirectory + s"/${name}_bright_0.png")
+    }
+  }
 }
+
