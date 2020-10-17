4b48311263ba95adaa0b65735dd355a8bc86c02f
lukaszlepak <lukasz.lepak@protonmail.com>
2020-10-17 22:12:32 +0200

    Add parallelism

--- src/main/scala/DarkImageFilter.scala
+++ src/main/scala/DarkImageFilter.scala
@@ -4,6 +4,9 @@ import com.sksamuel.scrimage.ImmutableImage
 import com.sksamuel.scrimage.nio.{JpegWriter, PngWriter}
 import com.typesafe.config.ConfigFactory
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 
 case class Config(inputDirectory: String, outputDirectory: String, threshold: Int)
 
@@ -11,25 +14,15 @@ case class Config(inputDirectory: String, outputDirectory: String, threshold: In
 object DarkImageFilter extends App {
 
   def imageDarkness(image: ImmutableImage): Int = {
-    val colors = image.colors()
+    val colors = image.colors
     val brightness = colors.foldLeft(0.0)((sum, color) => sum + color.blue*0.0722 + color.green*0.7152 + color.red*0.2126) / colors.size
     100 - (brightness * 100 / 255).toInt
   }
 
-  val configFile = ConfigFactory.load("application.conf")
-
-  val config = Config(
-    configFile.getString("dark-image-filter.input-directory"),
-    configFile.getString("dark-image-filter.output-directory"),
-    configFile.getInt("dark-image-filter.threshold")
-  )
-
-  new File(config.inputDirectory).listFiles().foreach { file =>
-    val (name, extension) = file.getName.split('.') match {
+  def processImage(nameWithExtension: String, image: ImmutableImage): Future[Unit] = Future {
+    val (name, extension) = nameWithExtension.split('.') match {
       case Array(n, e) => (n, e)
     }
-
-    val image = ImmutableImage.loader().fromFile(file)
 
     val imageDarknessScore = imageDarkness(image)
 
@@ -45,6 +38,18 @@ object DarkImageFilter extends App {
       case "jpeg" => image.output(JpegWriter.Default, config.outputDirectory + s"/$imageNameWithScore.jpeg")
       case "png" => image.output(PngWriter.NoCompression, config.outputDirectory + s"/$imageNameWithScore.png")
     }
+  }
+
+  val configFile = ConfigFactory.load("application.conf")
+
+  val config = Config(
+    configFile.getString("dark-image-filter.input-directory"),
+    configFile.getString("dark-image-filter.output-directory"),
+    configFile.getInt("dark-image-filter.threshold")
+  )
+
+  new File(config.inputDirectory).listFiles().foreach { file =>
+    processImage(file.getName, ImmutableImage.loader().fromFile(file))
   }
 }
 
