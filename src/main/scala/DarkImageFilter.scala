1165ad9bd59ce9d27b41294041f93d6c6add0f16
lukaszlepak <lukasz.lepak@protonmail.com>
2020-10-17 02:10:11 +0200

    Add darkness score algorithm

--- src/main/scala/DarkImageFilter.scala
+++ src/main/scala/DarkImageFilter.scala
@@ -5,16 +5,23 @@ import com.sksamuel.scrimage.nio.{JpegWriter, PngWriter}
 import com.typesafe.config.ConfigFactory
 
 
-case class Config(inputDirectory: String, outputDirectory: String)
+case class Config(inputDirectory: String, outputDirectory: String, threshold: Int)
 
 
 object DarkImageFilter extends App {
+
+  def imageDarkness(image: ImmutableImage): Int = {
+    val colors = image.colors()
+    val brightness = colors.foldLeft(0.0)((sum, color) => sum + color.blue*0.0722 + color.green*0.7152 + color.red*0.2126) / colors.size
+    100 - (brightness * 100 / 255).toInt
+  }
 
   val configFile = ConfigFactory.load("application.conf")
 
   val config = Config(
     configFile.getString("dark-image-filter.input-directory"),
-    configFile.getString("dark-image-filter.output-directory")
+    configFile.getString("dark-image-filter.output-directory"),
+    configFile.getInt("dark-image-filter.threshold")
   )
 
   new File(config.inputDirectory).listFiles().foreach { file =>
@@ -24,10 +31,19 @@ object DarkImageFilter extends App {
 
     val image = ImmutableImage.loader().fromFile(file)
 
+    val imageDarknessScore = imageDarkness(image)
+
+    val imageNameWithScore = {
+      if (imageDarknessScore < config.threshold)
+        s"/${name}_bright_$imageDarknessScore"
+      else
+        s"/${name}_dark_$imageDarknessScore"
+    }
+
     extension match {
-      case "jpg" => image.output(JpegWriter.Default, config.outputDirectory + s"/${name}_bright_0.jpg")
-      case "jpeg" => image.output(JpegWriter.Default, config.outputDirectory + s"/${name}_bright_0.jpeg")
-      case "png" => image.output(PngWriter.NoCompression, config.outputDirectory + s"/${name}_bright_0.png")
+      case "jpg" => image.output(JpegWriter.Default, config.outputDirectory + s"/$imageNameWithScore.jpg")
+      case "jpeg" => image.output(JpegWriter.Default, config.outputDirectory + s"/$imageNameWithScore.jpeg")
+      case "png" => image.output(PngWriter.NoCompression, config.outputDirectory + s"/$imageNameWithScore.png")
     }
   }
 }
